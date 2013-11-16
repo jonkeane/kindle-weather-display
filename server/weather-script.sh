9042c41980b2f88fdf5e9b2ebba0821d4c3948a0
Jonathan Keane <jkeane@gmail.com>
2013-11-16 12:26:30 -0600

    rotated the svg?

--- server/weather-script.sh
+++ server/weather-script.sh
@@ -3,6 +3,7 @@
 cd "$(dirname "$0")"
 
 python2 weather-transit-script.py
-rsvg-convert --background-color=white -rotate 90 -o weather-script-output.png weather-script-output.svg
-pngcrush -c 0 -ow weather-script-output.png
-cp -f weather-script-output.png /path/to/web/server/directory/weather-script-output.png
+rsvg-convert --background-color=white -o weather-script-output.png weather-script-output.svg
+convert weather-script-output.png -rotate 90 weather-script-output-rotated.png
+pngcrush -c 0 weather-script-output-rotated.png weather-script-output-final.png
+cp -f weather-script-output-final.png /path/to/web/server/directory/weather-script-output.png
