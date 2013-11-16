40f4dfd9c24c2577a9e7c50c56da15ea8090ecc3
Jonathan Keane <jkeane@gmail.com>
2013-11-16 12:29:49 -0600

    changed path of weberver to /var/www

--- server/weather-script.sh
+++ server/weather-script.sh
@@ -6,4 +6,4 @@ python2 weather-transit-script.py
 rsvg-convert --background-color=white -o weather-script-output.png weather-script-output.svg
 convert weather-script-output.png -rotate 90 weather-script-output-rotated.png
 pngcrush -c 0 weather-script-output-rotated.png weather-script-output-final.png
-cp -f weather-script-output-final.png /path/to/web/server/directory/weather-script-output.png
+cp -f weather-script-output-final.png /var/www/weather-script-output.png
