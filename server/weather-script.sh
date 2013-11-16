ec1f992200d4a7f1f99524752caafaaa642af05c
Jonathan Keane <jkeane@gmail.com>
2013-11-16 12:05:30 -0600

    changed weather-script.sh to point to the transit script

--- server/weather-script.sh
+++ server/weather-script.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")"
 
-python2 weather-script.py
+python2 weather-transit-script.py
 rsvg-convert --background-color=white -o weather-script-output.png weather-script-output.svg
 pngcrush -c 0 -ow weather-script-output.png
 cp -f weather-script-output.png /path/to/web/server/directory/weather-script-output.png
