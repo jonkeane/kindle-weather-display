06037aac954f8a72f0403b334c76859c711ccfa4
Jonathan Keane <jkeane@gmail.com>
2013-11-16 18:02:45 -0600

    changed the display script to allow for hourly and current

--- kindle/display-weather.sh
+++ kindle/display-weather.sh
@@ -2,12 +2,58 @@
 
 cd "$(dirname "$0")"
 
-rm weather-script-output.png
-eips -c
+rm weather-script-output-current.png
+rm weather-script-output-hourly.png
+#eips -c
 eips -c
 
-if wget http://server/path/to/weather-script-output.png; then
-	eips -g weather-script-output.png
+wget http://192.168.100.111:8080/weather-script-output-current.png
+wget http://192.168.100.111:8080/weather-script-output-hourly.png
+
+eips -c
+eips -c
+if [ -f weather-script-output-current.png ]; then
+	eips -g weather-script-output-current.png
+else
+	eips -g weather-image-error.png
+fi
+sleep 10
+
+eips -c
+if [ -f weather-script-output-hourly.png ]; then
+	eips -g weather-script-output-hourly.png
+else
+	eips -g weather-image-error.png
+fi
+sleep 10
+
+eips -c
+if [ -f weather-script-output-current.png ]; then
+	eips -g weather-script-output-current.png
+else
+	eips -g weather-image-error.png
+fi
+sleep 10
+
+eips -c
+if [ -f weather-script-output-hourly.png ]; then
+	eips -g weather-script-output-hourly.png
+else
+	eips -g weather-image-error.png
+fi
+sleep 10
+
+eips -c
+if [ -f weather-script-output-current.png ]; then
+	eips -g weather-script-output-current.png
+else
+	eips -g weather-image-error.png
+fi
+sleep 10
+
+eips -c
+if [ -f weather-script-output-hourly.png ]; then
+	eips -g weather-script-output-hourly.png
 else
 	eips -g weather-image-error.png
 fi
