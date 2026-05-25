4889ec37e86868f688340d8c5679c30f63a21f1b
Jonathan Keane <jkeane@gmail.com>
2026-05-25 14:11:46 -0500

    refactor the shell

--- server/weather-script.sh
+++ server/weather-script.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
+set -eu
+
 cd "$(dirname "$0")"
 
 python3 weather_transit.py
 
-rsvg-convert --background-color=white -o weather-script-output-current.png outputData/weather-script-output-current.svg
-convert weather-script-output-current.png -rotate 270 weather-script-output-current-rotated.png
-pngcrush -c 0 weather-script-output-current-rotated.png weather-script-output-current-final.png
-cp -f weather-script-output-current-final.png /var/www/weather-script-output-current.png
+output_dir="outputData"
 
+render_view() {
+    view="$1"
+    svg_path="$output_dir/weather-script-output-$view.svg"
+    rendered_png="$output_dir/weather-script-output-$view-rendered.png"
+    rotated_png="$output_dir/weather-script-output-$view-rotated.png"
+    final_png="$output_dir/weather-script-output-$view.png"
 
-rsvg-convert --background-color=white -o weather-script-output-hourly.png outputData/weather-script-output-hourly.svg
-convert weather-script-output-hourly.png -rotate 270 weather-script-output-hourly-rotated.png
-pngcrush -c 0 weather-script-output-hourly-rotated.png weather-script-output-hourly-final.png
-cp -f weather-script-output-hourly-final.png /var/www/weather-script-output-hourly.png
+    rsvg-convert --background-color=white -o "$rendered_png" "$svg_path"
+    convert "$rendered_png" -rotate 270 "$rotated_png"
+    pngcrush -c 0 "$rotated_png" "$final_png"
+    cp -f "$final_png" "/var/www/weather-script-output-$view.png"
+}
 
-rsvg-convert --background-color=white -o weather-script-output-daily.png outputData/weather-script-output-daily.svg
-convert weather-script-output-daily.png -rotate 270 weather-script-output-daily-rotated.png
-pngcrush -c 0 weather-script-output-daily-rotated.png weather-script-output-daily-final.png
-cp -f weather-script-output-daily-final.png /var/www/weather-script-output-daily.png
+for view in current hourly daily; do
+    render_view "$view"
+done
